0bb8dd10a70299da69b42cf0ac40666158e5bd0a
nathan spindel <nathans@gmail.com>
2008-05-24 19:21:23 -0700

    only look for today's events

--- CalCalendarStoreAdditions.m
+++ CalCalendarStoreAdditions.m
@@ -5,19 +5,17 @@
 
 @implementation CalCalendarStore (CalCalendarStoreAdditions)
 
-// return all events from (now - 20 mins) to (now + 24 hours)
+// return all non-allday events from 20 minutes ago to midnight
 + (NSArray *)eventsOccurringToday {
 	CalCalendarStore *defaultStore = [self defaultCalendarStore];
 	
-	NSPredicate *todayPredicate = [self eventPredicateWithStartDate:[NSDate dateWithTimeIntervalSinceNow:-1200] endDate:[NSDate dateWithTimeIntervalSinceNow:60*60*24] calendars:[defaultStore calendars]];
+	NSPredicate *todayPredicate = [self eventPredicateWithStartDate:[NSDate dateWithTimeIntervalSinceNow:-660] endDate:[NSDate dateWithNaturalLanguageString:@"tomorrow at midnight"] calendars:[defaultStore calendars]];
 	NSArray *todaysEvents = [defaultStore eventsWithPredicate:todayPredicate];
 
 	NSMutableArray *events = [NSMutableArray array];
-	for (CalEvent *potentialEvent in todaysEvents) {
-		if (!potentialEvent.isAllDay && [potentialEvent.startDate timeIntervalSinceDate:[NSDate date]] > -660) {
+	for (CalEvent *potentialEvent in todaysEvents)
+		if (!potentialEvent.isAllDay && [potentialEvent.startDate timeIntervalSinceDate:[NSDate date]] > -660)
 			[events addObject:potentialEvent];
-		}
-	}
 	
 	return events;
 }
